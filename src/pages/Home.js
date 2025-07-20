33095fe100e4ccd38ccebc215b16d64e3c945a46
PrashantTrivediKsolves <prashant.kumar@ksolves.com>
2025-07-21 00:48:45 +0530

    changes the name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/PrashantTrivediKsolves">Coder's Gyan</a>
+                    <a href="https://github.com/PrashantTrivediKsolves"> Prashant Trivedi</a>
                 </h4>
             </footer>
         </div>
